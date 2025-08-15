f34089bd9b09c7bcc0f67404f03d2dd3a813c6b0
siam9911 <155415462+siam9911@users.noreply.github.com>
2025-08-15 14:08:44 +0600

    3 done

--- javascript/src/App.jsx
+++ javascript/src/App.jsx
@@ -60,9 +60,9 @@ export default function App() {
     // Lesson 2
     show("a2", 10 + 3 * 8 + 5);
     show("b2", 39 / 3);
-    show("c2", (1850 + 750*2) / 100);
-    show("d2", 27 * 0.1);
-    show("e2", ((1850 + 750) * 0.2) / 100);
+    show("c2", (1850 + 750 * 2) / 100);
+    show("d2", 33.5 * 0.1);
+    show("e2", ((1850 + 750 * 2) * 0.2) / 100);
     show("f2", (1899 + 2095 + 799) / 100);
     const total = 1899 + 2095 + 799 + 499;
     show("g2", total / 100);
@@ -77,18 +77,37 @@ export default function App() {
     show("b3", "Siam");
     show("c3", "My name is:" + "Siam");
     show("d3", "Total cost: $" + (5 + 3));
+    show("e3", `Total cost: $${5 + 3}`);
+    click("f3", () => {
+      alert(`Total cost: $${5 + 3}`);
+    });
+    show("g3", "Total cost: " + (599+295)/100);
+    show("h3", `Total cost: $${(599+295)/100}`);
+    click("i3", () => {
+      alert(`Total cost: $${(599+295)/100}`)
+    })
+    click("j3", () => {
+      alert(`Total cost: $${(599+295)/100}
+Thank you, come again!`)
+    })
+    show("k3",`Items(${(2+2)}):${(2095+2095+799+799)/100}`)
+    show("l3",`Shipping and handling: $${(499+499)/100}`)
+    show("m3",`Total before tax: $${(5788+998)/100}`)
+    show("n3",`Estimated tax(10%):$${(6786*0.1)/100}`)
   }, []);
 
   return (
     <>
       <Header text="JavaScript" />
       <main className="container">
+        {/* Lesson 1 */}
         <section>
           <h2 className="section-title">Lesson 1</h2>
           <div className="section-container">
+            {/* Page 2 */}
             <button
               id="a1"
-              className="mx-auto block border rounded px-3 py-1 cursor-pointer"
+              className="mx-auto block border rounded px-3 py-1 mt-3 cursor-pointer"
             >
               1a. Show Alert
             </button>
@@ -117,11 +136,12 @@ export default function App() {
               1e. Show 2 + 2 - 5 in Console
             </button>
 
+            {/* Page 2 */}
             <button
               id="f1"
               className="mx-auto block border border-red-500 rounded px-3 py-1 mt-3 cursor-pointer"
             >
-              1f. Erase Page & Show Starting Date
+              1f. Erase Page & Show Good Morning
             </button>
             <button
               id="g1"
@@ -143,75 +163,249 @@ export default function App() {
             </button>
             <button
               id="j1"
-              className="mx-auto block border border-red-500 rounded px-3 py-1 mt-3 cursor-pointer"
+              className="mx-auto block border border-red-500 rounded px-3 py-1 my-3 cursor-pointer"
             >
               1j. Make Web Page Blank
             </button>
           </div>
         </section>
+
+        {/* Lesson 2 */}
         <section>
           <h2 className="section-title">Lesson 2</h2>
           <div className="section-container">
-            <p className="px-2 py-1 my-2 rounded border border-white">
-              2a. Cost of my order is: $<span id="a2" className="font-bold"></span>
+            {/* Page 3 */}
+            <p className="border px-3 py-1 m-3 rounded">
+              2a. Cost of my Order is: $
+              <span id="a2" className="font-bold"></span>
             </p>
-            <p className="px-2 py-1 my-2 rounded border border-white ">
-            2b. Cost of each Friend: $
-            <span id="b2" className="font-bold"></span>
+            <p className="border px-3 py-1 m-3 rounded">
+              2b. Cost of each Friend: $
+              <span id="b2" className="font-bold"></span>
             </p>
-            <p className="px-2 py-1 my-2 rounded border border-white ">
-            2c. Total cost of a toaster and 2 shirts: $
-            <span id="c2" className="font-bold"></span>
+            <p className="border px-3 py-1 m-3 rounded">
+              2c. Total cost of a toaster and 2 shirts: $
+              <span id="c2" className="font-bold"></span>
             </p>
-            <p className="px-2 py-1 my-2 rounded border border-white ">
-            2d. 10% Tax of Total Cost: $
-            <span id="d2" className="font-bold"></span>
+            <p className="border px-3 py-1 m-3 rounded">
+              2d. 10% Tax of Total Cost: $
+              <span id="d2" className="font-bold"></span>
             </p>
-            <p className="px-2 py-1 my-2 rounded border border-white ">
-            2e. 20% Tax of Total Cost: $
-            <span id="e2" className="font-bold"></span>
+            <p className="border px-3 py-1 m-3 rounded">
+              2e. 20% Tax of Total Cost: $
+              <span id="e2" className="font-bold"></span>
             </p>
-            <p className="px-2 py-1 my-2 rounded border border-white ">
-            2f. Cost without Tax and Shipping: $
-            <span id="f2" className="font-bold"></span>
+
+            {/* Page 4 */}
+            <p className="border px-3 py-1 m-3 rounded">
+              2f. Cost without Tax and Shipping: $
+              <span id="f2" className="font-bold"></span>
             </p>
-            <p className="px-2 py-1 my-2 rounded border border-white ">
-              2g. Cost without Tax: $
-            <span id="g2" className="font-bold"></span>
+            <p className="border px-3 py-1 m-3 rounded">
+              2g. Cost without Tax: $<span id="g2" className="font-bold"></span>
             </p>
-            <p className="px-2 py-1 my-2 rounded border border-white ">
-              2h. Tax: $
-            <span id="h2" className="font-bold"></span>
+            <p className="border px-3 py-1 m-3 rounded">
+              2h. Tax: $<span id="h2" className="font-bold"></span>
             </p>
-            <p className="px-2 py-1 my-2 rounded border border-white ">
+            <p className="border px-3 py-1 m-3 rounded">
               2i. Total Payable Amount: $
-            <span id="i2" className="font-bold"></span>
+              <span id="i2" className="font-bold"></span>
             </p>
-            <p className="px-2 py-1 my-2 rounded border border-white ">
-              2j. Rounding a number down(2.8):
-            <span id="j2" className="font-bold"></span>
+
+            {/* Page 5 */}
+            <p className="border px-3 py-1 m-3 rounded">
+              2j. Rounding a number down (2.8):{" "}
+              <span id="j2" className="font-bold"></span>
             </p>
-            <p className="px-2 py-1 my-2 rounded border border-white ">
-              2k. Rounding a number up(2.2):
-            <span id="k2" className="font-bold"></span>
+            <p className="border px-3 py-1 m-3 rounded">
+              2k. Rounding a number up (2.2):{" "}
+              <span id="k2" className="font-bold"></span>
             </p>
-            <p className="px-2 py-1 my-2 rounded border border-white ">
-            2l. 25°C = °F
-            <span id="l2" className="font-bold"></span>
+            <p className="border px-3 py-1 m-3 rounded">
+              2l. 25°C = <span id="l2" className="font-bold"></span>°F
             </p>
-            <p className="px-2 py-1 my-2 rounded border border-white ">
-            2m. 86°F =  °C
-            <span id="m2" className="font-bold"></span>
+            <p className="border px-3 py-1 m-3 rounded">
+              2m. 86°F = <span id="m2" className="font-bold"></span>°C
             </p>
-            <p className="px-2 py-1 my-2 rounded border border-white ">
-            2n. -5°C = °F
-            <span id="n2" className="font-bold"></span>
+            <p className="border px-3 py-1 m-3 rounded">
+              2n. -5°C = <span id="n2" className="font-bold"></span>°F
             </p>
-            </div>
+          </div>
         </section>
+
+        {/* Lesson 3 */}
         <section>
           <h2 className="section-title">Lesson 3</h2>
-          <div className="section-container"></div>
+          <div className="section-container">
+            {/* Page 6 */}
+            <p className="border px-3 py-1 m-3 rounded">
+              3a. Creating a string :{" "}
+              <span id="a3" className="font-bold"></span>
+            </p>
+            <p className="border px-3 py-1 m-3 rounded">
+              3b. Creating Name as a string :{" "}
+              <span id="b3" className="font-bold"></span>
+            </p>
+            <p className="border px-3 py-1 m-3 rounded">
+              3c. Concatenating 3a and 3b :{" "}
+              <span id="c3" className="font-bold"></span>
+            </p>
+            <p className="border px-3 py-1 m-3 rounded">
+              3d. Created Text : <span id="d3" className="font-bold"></span>
+            </p>
+            <p className="border px-3 py-1 m-3 rounded">
+              3e. Created Text using template string :{" "}
+              <span id="e3" className="font-bold"></span>
+            </p>
+            <button
+              id="f3"
+              className="mx-auto block cursor-pointer border px-3 py-1 m-3 rounded"
+            >
+              3f. Show in Alert
+            </button>
+            
+            {/* Page 7 */}
+            <p className="border px-3 py-1 m-3 rounded">
+              3g. Created Text2 : <span id="g3" className="font-bold"></span>
+            </p>
+            <p className="border px-3 py-1 m-3 rounded">
+              3h. Created Text2 using template string :{" "}
+              <span id="h3" className="font-bold"></span>
+            </p>
+            <button
+              id="i3"
+              className="mx-auto block cursor-pointer border px-3 py-1 m-3 rounded"
+            >
+              3i. Show in Alert
+            </button>
+            <button
+              id="j3"
+              className="mx-auto block cursor-pointer border px-3 py-1 m-3 rounded"
+            >
+              3j. Show Multiline String
+            </button>
+
+            {/* Page 8 */}
+            <p className="border px-3 py-1 m-3 rounded">
+              3k. First Line : <span id="k3" className="font-bold"></span>
+            </p>
+            <p className="border px-3 py-1 m-3 rounded">
+              3l. Second Line : <span id="l3" className="font-bold"></span>
+            </p>
+            <p className="border px-3 py-1 m-3 rounded">
+              3m. Third Line : <span id="m3" className="font-bold"></span>
+            </p>
+            <p className="border px-3 py-1 m-3 rounded">
+              3n. Fourth Line : <span id="n3" className="font-bold"></span>
+            </p>
+          </div>
+        </section>
+
+        {/* Lesson 4 */}
+        <section>
+          <h2 className="section-title">Lesson 4</h2>
+          <div className="section-container">
+            {/* Page 9 */}
+            <button className="mx-auto block cursor-pointer border px-3 py-1 m-3 rounded">
+              4a. Click
+            </button>
+            <div className="mx-auto w-fit">
+              <button className="cursor-pointer border px-3 py-1 m-3 rounded">
+                4b. Nuru
+              </button>
+              <button className="cursor-pointer border px-3 py-1 m-3 rounded">
+                4b. Biriyani
+              </button>
+            </div>
+            <p className="border px-3 py-1 m-3 rounded">4c. Hello, world!</p>
+            <div className="border px-3 py-1 m-3 rounded">
+              <p className="py-1">4d. Hello, world!</p>
+              <p className="py-1">
+                4d. Today I woke up in the morning and started writing code.
+              </p>
+            </div>
+
+            {/* Page 10 */}
+            <div className="mx-auto w-fit">
+              <button className="cursor-pointer border px-3 py-1 m-3 rounded bg-yellow-300">
+                4e. Add to cart
+              </button>
+              <button className="cursor-pointer border px-3 py-1 m-3 rounded bg-orange-500">
+                4e. Buy now
+              </button>
+            </div>
+            <p className="border px-3 py-1 m-3 rounded">4f. Already Done !!</p>
+            <p className="border px-3 py-1 m-3 rounded">4g. Already Done !!</p>
+            <button
+              id="h4"
+              className="mx-auto block cursor-pointer border px-3 py-1 m-3 rounded"
+            >
+              4h. Show in Console
+            </button>
+
+            {/* Page 11 */}
+            <div className="border px-3 py-1 m-3 rounded">
+              <p className="py-1">4i. Adults Plain Cotton T-shirt</p>
+              <p className="py-1">4i. Price: $7.99</p>
+              <button
+                className="cursor-pointer border px-3 py-2 my-3 rounded bg-yellow-300"
+                onClick={() => {
+                  alert("Added!");
+                }}
+              >
+                4j. Add to cart
+              </button>
+              <button
+                className="cursor-pointer border px-3 py-2 m-3 rounded bg-orange-500"
+                onClick={() => {
+                  console.log("Loading...");
+                  alert("Purchased!");
+                }}
+              >
+                4j. Buy now
+              </button>
+            </div>
+          </div>
+        </section>
+
+        {/* Lesson 5 */}
+        <section>
+          <h2 className="section-title">Lesson 5</h2>
+          <div className="section-container">
+            {/* Page 12 */}
+            <p className="border px-3 py-1 m-3 rounded">
+              5a. Variable Created !!
+            </p>
+            <button
+              id="b5"
+              className="mx-auto block cursor-pointer border px-3 py-1 m-3 rounded"
+            >
+              5b. Display in the console
+            </button>
+            <p className="border px-3 py-1 m-3 rounded">
+              5c. Saved in variable !!
+            </p>
+            <button
+              id="d5"
+              className="mx-auto block cursor-pointer border px-3 py-1 m-3 rounded"
+            >
+              5d. Show in the console
+            </button>
+            <p className="border px-3 py-1 m-3 rounded">
+              5e. Saved in variable !!
+            </p>
+
+            {/* Page 13: Lesson 5 */}
+          </div>
+        </section>
+
+        <section>
+          <h2 className="section-title">Lesson 6</h2>
+          <div className="section-container">
+            <h4>Up Next...</h4>
+            <h4>Up Next...</h4>
+            <h4>Up Next...</h4>
+          </div>
         </section>
       </main>
       <Footer name="Siam" />
